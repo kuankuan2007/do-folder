3f8614b16d2ca2e8e5835a151bfdd76a2a176ff5
kuankuan2007 <2163826131@qq.com>
2025-06-27 14:20:33 +0800

    change version to 2.3.0-alpha.1

--- src/doFolder/__pkginfo__.py
+++ src/doFolder/__pkginfo__.py
@@ -3,4 +3,4 @@ This is the package information module for the doFolder library.
 It contains metadata about the package, such as its version.
 """
 
-__version__ = "2.2.2-post1"
+__version__ = "2.3.0-alpha.1"
